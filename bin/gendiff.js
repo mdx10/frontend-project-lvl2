ee3810cde702199f8048957e63a01d66df7c2e6d
Aleksandr Smirnov <winst500@gmail.com>
2021-11-16 03:38:47 +0700

    add commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+
+const programm = new Command();
+programm
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
